c0672fd50258d4a5e441e45388873f485b854bf6
Panda115 <117127543+Panda115@users.noreply.github.com>
2022-12-14 00:52:42 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from "@angular/core";
-import { ProductService } from "./productservice";
-import { Product } from "./product";
+import { Component, ViewChild } from '@angular/core';
+import { ProductService } from './productservice';
+import { Table } from 'primeng/table';
+import { Product } from './product';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html"
+  selector: 'app-root',
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   products: Product[];
+  @ViewChild('dt') dt: Table;
 
   selectedProducts: Product[];
 
@@ -18,44 +20,52 @@ export class AppComponent {
   exportColumns: any[];
 
   ngOnInit() {
-    this.productService.getProductsSmall().then(data => (this.products = data));
+    this.productService
+      .getProductsSmall()
+      .then((data) => (this.products = data));
 
     this.cols = [
-      { field: "code", header: "Code" },
-      { field: "name", header: "Name" },
-      { field: "category", header: "Category" },
-      { field: "quantity", header: "Quantity" }
+      { field: 'code', header: 'Code' },
+      { field: 'name', header: 'Name' },
+      { field: 'category', header: 'Category' },
+      { field: 'quantity', header: 'Quantity' },
     ];
 
-    this.exportColumns = this.cols.map(col => ({
+    this.exportColumns = this.cols.map((col) => ({
       title: col.header,
-      dataKey: col.field
+      dataKey: col.field,
     }));
   }
 
+  exportCsv() {
+    this.dt.exportCSV();
+    
+  }
+
   exportExcel() {
-    import("xlsx").then(xlsx => {
+    import('xlsx').then((xlsx) => {
       const worksheet = xlsx.utils.json_to_sheet(this.products);
-      const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
       const excelBuffer: any = xlsx.write(workbook, {
-        bookType: "xlsx",
-        type: "array"
+        bookType: 'xlsx',
+        type: 'array',
       });
-      this.saveAsExcelFile(excelBuffer, "products");
+      this.saveAsExcelFile(excelBuffer, 'products');
     });
   }
 
   saveAsExcelFile(buffer: any, fileName: string): void {
-    import("file-saver").then(FileSaver => {
+    import('file-saver').then((FileSaver) => {
       let EXCEL_TYPE =
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-      let EXCEL_EXTENSION = ".xlsx";
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+      let EXCEL_EXTENSION = '.xlsx';      
+
       const data: Blob = new Blob([buffer], {
-        type: EXCEL_TYPE
+        type: EXCEL_TYPE,
       });
       FileSaver.saveAs(
         data,
-        fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+        fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION
       );
     });
   }
